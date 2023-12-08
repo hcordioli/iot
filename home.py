766da1c155b586252e5d5628bd63315e56bc5054
Humberto Cordioli <hcordioli@gmail.com>
2023-12-08 09:36:44 -0300

    Add files via upload

---
+++ home.py
@@ -0,0 +1,21 @@
+""" IoT Home Page 
+
+Página inicial do Piloto IoT Dashboard.
+
+"""
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Dashboard IoT",
+    page_icon="🔍",
+)
+
+st.write("# Bem vindo ao Dashboard IoT 🔍")
+
+st.markdown(
+    """
+    Página inicial do Dashboard IoT
+
+""", unsafe_allow_html=True
+)
