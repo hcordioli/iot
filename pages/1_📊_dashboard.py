f849f2e753e7e75594419406c4067e18c4086ccb
Humberto Cordioli <hcordioli@gmail.com>
2023-12-08 14:51:38 -0300

    Inclusão dos primeiros gráficos

--- pages/1_📊_dashboard.py
+++ pages/1_📊_dashboard.py
@@ -2,15 +2,19 @@ import streamlit as st
 import os
 import requests
 import boto3
+from boto3.dynamodb.conditions import Key
+from decimal import *
 import time
 import datetime
+import numpy as np
 import pandas as pd
+import plotly.express as px 
 
 # Query range
 start_date = "01/12/2023"
 my_time = time.strptime(start_date, "%d/%m/%Y")
-start_date_timestamp = int(time.mktime(my_time))
-today_date_timestamp = int(time.time())
+start_date_timestamp = int(time.mktime(my_time)) * 1000
+today_date_timestamp = int(time.time()) * 1000
 
 # Table info
 IOT_TABLE_NAME = "iot_data_ddb"
@@ -38,8 +42,30 @@ def load_iot_table():
     iot_df['temperature'] = iot_df['device_data'].map(lambda d : d['temperature'])
     iot_df['light'] = iot_df['device_data'].map(lambda d : d['light'])
     iot_df = iot_df.drop(columns=["device_data"])
-
+    iot_df['sample_time'] = iot_df['sample_time'].astype(np.int64)
+    iot_df['sample_time'] = pd.to_datetime(iot_df['sample_time'],unit="ms")
+    iot_df = iot_df.sort_values("sample_time",ascending=True)
     return (iot_df)
 
 iot_df = load_iot_table()
-st.write(iot_df)
+
+# Presentation
+temp_col, light_col = st.columns(2,gap="small")
+
+# Temperature
+fig_temp = px.line(
+    iot_df,
+    x='sample_time',
+    y="temperature",
+    title="Temperatura"
+)
+temp_col.plotly_chart(fig_temp,use_container_width=True)
+
+# Temperature
+fig_light = px.line(
+    iot_df,
+    x='sample_time',
+    y="light",
+    title="Luminosidade"
+)
+light_col.plotly_chart(fig_light,use_container_width=True)
